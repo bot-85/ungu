f639b7fdb6ff064377e620a62e26b148bada275f
BINTANG-XD <94216075+bot-85@users.noreply.github.com>
2021-11-26 20:45:17 +0700

    Update ungu.py

--- ungu.py
+++ ungu.py
@@ -878,6 +878,7 @@ def menu():
         exit("%s [!] Kesalahan koneksi "%(M))
     banner()
     print ('\n %s[ Selamat kepada %s%s%s ] \n'%(P,H,nama,P))
+    print '___________________________________________________________\n';time.sleep(0.03)
     print (' [%s01%s]. Crack Nama Ig/Instagram'%(O,U)) 
     print (' [%s02%s]. Dump Id Facebook Publik'%(O,K)) 
     print (' [%s03%s]. Dump Id Facebook Follow'%(O,U))
