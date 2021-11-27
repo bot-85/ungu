47e3cfef14fa4041adc69b5c2f78167f91f30072
BINTANG-XD <94216075+bot-85@users.noreply.github.com>
2021-11-27 14:15:56 +0700

    Update ungu.py

--- ungu.py
+++ ungu.py
@@ -81,8 +81,8 @@ def masuk():
             gas = requests.get('https://graph.facebook.com/me?access_token=%s'%(romz)).json()['name']
             print ('\n%s[√] Login berhasil. '%(H));jeda(2)
             open('token.txt', 'w').write(romz);login_xx()
+      except (KeyError,IOError):
         	print("%s [!] Token invalid "%(M));masuk()
-                kecuali ( KeyError , IOError ):
     elif rom in ('2', '02'):
     	print ("\n%s%s Ikuti langkah langkah sebagai berikut"%(H,til));jeda(2)
         print (" - pertama masuk Google chrome, Usahkan akun tumbal ");jeda(2)
